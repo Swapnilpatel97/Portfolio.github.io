0d0ecd752d1bba15073de1937fe581ffd6e23567
Swapnil Patel <swapnilpatel212601@gmail.com>
2024-04-21 15:40:59 +0530

    Updated Project section

--- projects/script.js
+++ projects/script.js
@@ -44,46 +44,33 @@ function showProjects(projects) {
     let projectsContainer = document.querySelector(".work .box-container");
     let projectsHTML = "";
     projects.forEach(project => {
-        projectsHTML += `
-        <div class="grid-item ${project.category}">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-        <img draggable="false" src="\assets\images\projects${project.image}.png" alt="Error" />
+        // Appending file extension (.png) to the image filename
+        let imagePath = `../assets/images/projects/${project.image}.png`;
 
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
-        </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>`
+        // Constructing the HTML for each project
+        projectsHTML += `
+            <div class="grid-item ${project.category}">
+                <div class="box tilt" style="width: 380px; margin: 1rem">
+                    <img draggable="false" src="${imagePath}" alt="${project.name}" />
+
+                    <div class="content">
+                        <div class="tag">
+                            <h3>${project.name}</h3>
+                        </div>
+                        <div class="desc">
+                            <p>${project.desc}</p>
+                            <div class="btns">
+                                <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+                                <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+            </div>`;
     });
     projectsContainer.innerHTML = projectsHTML;
 
-    // vanilla tilt.js
-    // VanillaTilt.init(document.querySelectorAll(".tilt"), {
-    //     max: 20,
-    // });
-    // // vanilla tilt.js  
-
-    // /* ===== SCROLL REVEAL ANIMATION ===== */
-    // const srtop = ScrollReveal({
-    //     origin: 'bottom',
-    //     distance: '80px',
-    //     duration: 1000,
-    //     reset: true
-    // });
-
-    // /* SCROLL PROJECTS */
-    // srtop.reveal('.work .box', { interval: 200 });
-
-    // isotope filter products
+    // Initialize Isotope
     var $grid = $('.box-container').isotope({
         itemSelector: '.grid-item',
         layoutMode: 'fitRows',
@@ -92,7 +79,7 @@ function showProjects(projects) {
         }
     });
 
-    // filter items on button click
+    // Filter items on button click
     $('.button-group').on('click', 'button', function () {
         $('.button-group').find('.is-checked').removeClass('is-checked');
         $(this).addClass('is-checked');
--- script.js
+++ script.js
@@ -10,3 +10,55 @@ function SendMail()
         alert("Success !!");
     })
 }
+
+
+function showProjects(projects) {
+    let projectsContainer = document.querySelector(".work .box-container");
+    let projectsHTML = "";
+    projects.forEach(project => {
+        // Appending file extension (.png) to the image filename
+        let imagePath = `/assets/images/projects/${project.image}.png`;
+
+        // Constructing the HTML for each project
+        projectsHTML += `
+            <div class="grid-item ${project.category}">
+                <div class="box tilt" style="width: 380px; margin: 1rem">
+                    <img draggable="false" src="${imagePath}" alt="${project.name}" />
+
+                    <div class="content">
+                        <div class="tag">
+                            <h3>${project.name}</h3>
+                        </div>
+                        <div class="desc">
+                            <p>${project.desc}</p>
+                            <div class="btns">
+                                <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+                                <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+            </div>`;
+    });
+    projectsContainer.innerHTML = projectsHTML;
+
+    // Initialize Isotope
+    var $grid = $('.box-container').isotope({
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows',
+        masonry: {
+            columnWidth: 200
+        }
+    });
+
+    // Filter items on button click
+    $('.button-group').on('click', 'button', function () {
+        $('.button-group').find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+}
+getProjects().then(data => {
+    showProjects(data);
+})
